535d84b89a737effabfec750287d0b988c42d092
Adhish-Krishna <adhishthesak@gmail.com>
2025-02-27 19:05:27 +0530

    Update UI styles and adjust margins for auction components

--- frontend/codopoly/src/components/auction/CurrentPoints.jsx
+++ frontend/codopoly/src/components/auction/CurrentPoints.jsx
@@ -4,7 +4,7 @@ const Score = ()=>{
     return(
         <>
             <div className={styles.scorecontainer}>
-                <p className={styles.maintext}>Your Score</p>
+                <p style={{"margin":10}} className={styles.maintext}>Your Score</p>
                 <p className={styles.score}>{/*Use a API call here to obtain the score*/}1000</p>
             </div>
         </>
