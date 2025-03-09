43aa9569a4802ba0e1ec72d32e1b3692bad60a28
arun <arun.akm137@gmail.com>
2025-03-09 12:30:13 +0530

    Added timer and homePage
    timer funtioned to reset every 60 seconds for 10 codes to be auctioned

--- frontend/codopoly/src/components/auction/AuctionCode.jsx
+++ frontend/codopoly/src/components/auction/AuctionCode.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect} from "react";
 import styles from '../../styles/bidding.module.css';
 
-const AuctionCode  = ()=>{
+const AuctionCode = ({ triggerFetch, codeIndex, maxCodes }) => {
+    const fetchAuctionCode = async () => {
+        if (codeIndex < maxCodes) {  
+            // API call to get code to auction
+        }
+    };
+
+    useEffect(() => {
+        if (codeIndex < maxCodes) {  
+            fetchAuctionCode();
+        } 
+    }, [triggerFetch, codeIndex, maxCodes]); // fetch called every time, timer resets
+    
     return(
         <>
             <div className={styles.auctioncodecontainer}>
                 <p style={{"margin":10}} className={styles.maintext}>Auction Code</p>
                 <div className={styles.auctioncodesubcontainer}>
-                    {/*Use a api call here to get the auction code*/}
+                    {/* Display fetches auction code */}
                 </div>
             </div>
 
--- frontend/codopoly/src/pages/HomePage.jsx
+++ frontend/codopoly/src/pages/HomePage.jsx
@@ -1,10 +1,30 @@
-const HomePage = ()=>{
-  return(
-    <>
-      <h1>This is the HomePage</h1>
-      <h1>Here there will be the instructions for the users about Codopoly</h1>
-    </>
-  )
-}
+import '../styles/homepage.css';
+import { useNavigate } from "react-router-dom";
 
-export default HomePage
+const HomePage = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <div className="container">
+                <header className="header">
+                    <h1 className="maintext">Welcome to Codopoly</h1>
+                </header>
+                <main className='content'>
+                    <p className="text">
+                        Codopoly is an exhilarating coding event consisting of three rounds. Each team starts with a main code
+                        segment and receives two additional code segments which belong to other teams. The objective is to debug these
+                        external codes and submit them to the auction. Success in Codopoly requires sharp debugging skills,
+                        strategic thinking, and effective collaboration.
+                    </p>
+                    <div className="buttonContainer">
+                        <button onClick={() => navigate('/debugging')} className="btn">Debug Code</button>
+                        <button onClick={() => navigate('/bidding')} className="btn">Enter Auction</button>
+                    </div>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default HomePage;
