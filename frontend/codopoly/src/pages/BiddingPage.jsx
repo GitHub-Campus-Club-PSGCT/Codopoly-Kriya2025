58d6295f0109ca5f370bfb8cc252ef8c180f7152
Dhaarun-Abhimanyu <dhaarunabhimanyu@gmail.com>
2025-03-09 09:33:21 +0000

    Resolved the auction timer

--- frontend/codopoly/src/pages/BiddingPage.jsx
+++ frontend/codopoly/src/pages/BiddingPage.jsx
@@ -3,23 +3,11 @@ import Bidding from "../components/auction/Bidding.jsx";
 import Header from "../components/auction/Header.jsx";
 import AuctionCode from "../components/auction/AuctionCode.jsx";
 import Score from '../components/auction/CurrentPoints.jsx';
-import Timer from '../components/auction/Timer.jsx';
 import { useState } from 'react';
-
-const MAX_CODES = 10; // Assuming 10 codes are to be auctioned , change as required
 
 const BiddingPage = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [mainPartOpen, setMainPartOpen] = useState(false);
-  const [fetchTrigger, setFetchTrigger] = useState(0)
-  const [codeIndex, setCodeIndex] = useState(0);
-
-  const handleTimerReset = () => {
-    if (codeIndex < MAX_CODES) {
-      setFetchTrigger(prev => prev + 1);
-      setCodeIndex(prev => prev + 1);
-    }
-  };
 
   return (
     <>
@@ -29,21 +17,16 @@ const BiddingPage = () => {
         mainPartOpen={mainPartOpen}
         setMainPartOpen={setMainPartOpen}
       />
-
       <div className={`${styles.container} ${sidebarOpen || mainPartOpen ? styles.narrowed : ''}`}>
-        <div className = {styles.timer}>
-          {codeIndex < MAX_CODES && <Timer onTimerReset={handleTimerReset} />}
-        </div>
         <div className={styles.scoreContainer}>
           <Score />
         </div>
         <div className={styles.contentContainer}>
-          <AuctionCode triggerFetch={fetchTrigger} codeIndex={codeIndex} maxCodes={MAX_CODES} />
+          <AuctionCode />
           <Bidding />
         </div>
       </div>
     </>
   )
 }
-
 export default BiddingPage;
