c1bab913be8401d27395e10f73f18339162443ed
DARSHAN-V-G <darshanguna22@gmail.com>
2025-03-13 07:42:21 +0530

    vercel deploye

--- frontend/codopoly/vite.config.js
+++ frontend/codopoly/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  port:5174,
-})
+  server: {
+    port: 5173, // Optional: Local development port
+  },
+  base: "/", // Ensures correct paths in production
+});
