2f7066516d280fadd64fb55bbf06dacb48ca6675
DARSHAN-V-G <darshanguna22@gmail.com>
2025-03-14 23:39:37 +0530

    rectfified deploy error

--- backend/routes/questionRoutes.js
+++ backend/routes/questionRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getQuestions,getPOC, submitQuestions } = require('../controllers/questionController');
+const { getQuestions,getPOC, submitQuestions,getTeamPOCduringAuction } = require('../controllers/questionController');
 const { protect } = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.get('/', getQuestions);
 router.post('/submit', submitQuestions);
 router.get('/getPOC/:pocName',getPOC);
-router.get('/poc', protect, getTeamPOC);
+router.get('/poc', protect, getTeamPOCduringAuction);
 module.exports = router;
